ab1ce274e50be8e5c2e8e5e3a00d296c30ce0e7f
Noura-no <146091963+Noura-no@users.noreply.github.com>
2024-03-10 23:18:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let Body = document.getElementById('body')
+
+dark.addEventListener('click', function(){
+    Body.style.backgroundColor ='#938c8c99'
+})
+light.addEventListener('click', function(){
+    Body.style.backgroundColor ='white'
+})
